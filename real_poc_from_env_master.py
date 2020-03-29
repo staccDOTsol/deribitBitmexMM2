68998d08765cd55246918ed84fea3181933d8674
Your Name <jare@beast-can-new.us-west1-b.c.coindex-ai-v0.internal>
2020-03-29 15:00:49 +0000

    posting

--- real_poc_from_env_master.py
+++ real_poc_from_env_master.py
@@ -863,8 +863,8 @@ class MarketMaker( object ):
 
                 spot            = self.get_spot()
                 bal_btc         = account[ 'equity' ] * 100
-                pos_lim_long    = bal_btc * (PCT_LIM_LONG * 0.75) / len(self.futures)
-                pos_lim_short   = bal_btc * (PCT_LIM_SHORT * 0.75) / len(self.futures)
+                pos_lim_long    = bal_btc * (PCT_LIM_LONG * 0.5) / len(self.futures)
+                pos_lim_short   = bal_btc * (PCT_LIM_SHORT * 0.5) / len(self.futures)
 
                 if 'PERPETUAL' in fut:
                     pos_lim_short = pos_lim_short * (len(self.futures)  - 1)
@@ -921,7 +921,7 @@ class MarketMaker( object ):
                     positionSize2 = positionSize2 + self.positions[p]['size']
                 if positionSize2 < 0:
                 	positionSize2 = positionSize2 * -1
-                if place_asks2 == False and positionSize2 > self.maxqty / 10 * 2.5 * 3.5:
+                if place_asks2 == False and positionSize2 > self.maxqty / 10 * 2.5 * 2.5:
                     if len(askso) >= 1:
                         asksn.append(askso[0])
                     if len(askso) >= 2:
@@ -935,7 +935,7 @@ class MarketMaker( object ):
                         bidsn.append(bids1[1])
                         nbids = nbids + 1
                     len_bid_ords = min( len( bid_ords ), nbids )
-                if place_bids2 == False and positionSize2 > self.maxqty / 10 * 2.5 * 3.5:
+                if place_bids2 == False and positionSize2 > self.maxqty / 10 * 2.5 * 2.5:
 	                if len(bidso) >= 1:
 	                    bidsn.append(bidso[0])
 	                if len(bidso) >= 2:
